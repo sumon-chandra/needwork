9864334e81b9d3989edbd215fa9b2444c531962f
Sumon Chandra Shil <topu199041@gmail.com>
2022-01-20 00:47:31 +0600

    added footer

--- src/components/Home/Testimonial.jsx
+++ src/components/Home/Testimonial.jsx
@@ -11,15 +11,15 @@ const Testimonial = () => {
           <div className="testimonials">
           <div
         id="carouselExampleIndicators"
-        class="carousel slide h-100"
+        className="carousel slide h-100"
         data-bs-ride="carousel"
       >
-        <div class="carousel-indicators">
+        <div className="carousel-indicators">
           <button
             type="button"
             data-bs-target="#carouselExampleIndicators"
             data-bs-slide-to="0"
-            class="active"
+            className="active"
             aria-current="true"
             aria-label="Slide 1"
           ></button>
@@ -36,22 +36,22 @@ const Testimonial = () => {
             aria-label="Slide 3"
           ></button>
         </div>
-        <div class="carousel-inner  h-100">
-          <div class="carousel-item position-relative active ">
+        <div className="carousel-inner  h-100">
+          <div className="carousel-item position-relative active ">
             <div className="carousel-text">
             <h5>Moshiour, He is a Young, Talented and Professional guy. He really helped us a lot to Digitize our business. We generated 3.5x client after taking services from Avista Digital . Best Wishes</h5>
             <h6>Abdul Hamid</h6>
             <p>Chairman, HB Logistic Bangladesh</p>
             </div>
           </div>
-          <div class="carousel-item  ">
+          <div className="carousel-item  ">
           <div className="carousel-text">
             <h5>Moshiour Rahman is the best guy for digital marketing I have been working with him since 2019 and getting a good response so far, my site is always showing the first page of Google I'm highly recommended him.</h5>
             <h6>Mike Devis</h6>
             <p>Founder Company Owner, NY</p>
             </div>
           </div>
-          <div class="carousel-item  ">
+          <div className="carousel-item  ">
             <div className="carousel-text">
             <h5>Moshiour Bhai is an excellent person from Avista Digital. He got my site ranked within 4 months. Avista Digital is the best SEO services company in Bangladesh. I recommend them for web design and SEO services. They helped me to get my logo, business website and SEO. They provide a complete business startup solution. I wish them Good Luck 🙂</h5>
             <h6>Arfin Nayeem Leo</h6>
@@ -60,22 +60,22 @@ const Testimonial = () => {
           </div>
         </div>
         <button
-          class="carousel-control-prev"
+          className="carousel-control-prev"
           type="button"
           data-bs-target="#carouselExampleIndicators"
           data-bs-slide="prev"
         >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
         </button>
         <button
-          class="carousel-control-next"
+          className="carousel-control-next"
           type="button"
           data-bs-target="#carouselExampleIndicators"
           data-bs-slide="next"
         >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
         </button>
       </div>
           </div>
--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,11 +1,118 @@
-
 import React from "react";
+import { Link } from "react-router-dom";
+import footerImg from "../../img/logo/logo02.png"
+import "./Footer.css"
 
 const Footer = () => {
   return (
     <div>
-        <h1>This is footer</h1>
-      
+      <footer className="">
+        <div className="footer-top ">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-3 col-sm-6">
+                <div className="address">
+                  <img src={footerImg} className="w-50" alt="" />
+                  <p className="mb-4 mt-4">Shahabad Markanda, 136135, <br /> India</p>
+                  <p>
+                    <strong>Phone:</strong> +91 8558020904
+                  </p>
+                  <p>
+                    <strong>Email:</strong> markuptag20@gmail.com
+                  </p>
+                </div>
+              </div>
+
+              <div className="col-lg-2 col-sm-6 footer-menus">
+                <h4>Useful Links</h4>
+                <ul>
+                  <li>
+                    <i className="fas fa-check"></i> <Link to="#">Home</Link>
+                  </li>
+                  <li>
+                    <i className="fas fa-check"></i>{" "}
+                    <Link to="#">About us</Link>
+                  </li>
+                  <li>
+                    <i className="fas fa-check"></i>{" "}
+                    <Link to="#">Services</Link>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="col-lg-3 col-sm-6 footer-menus">
+                <h4>Our Services</h4>
+                <ul>
+                  <li>
+                    <i className="fas fa-check"></i>{" "}
+                    <Link to="#">Web Design</Link>
+                  </li>
+                  <li>
+                    <i className="fas fa-check"></i>{" "}
+                    <Link to="#">Web Development</Link>
+                  </li>
+                  
+                  <li>
+                    <i className="fas fa-check"></i>{" "}
+                    <Link to="#">Digital Marketing</Link>
+                  </li>
+                  <li>
+                    <i className="fas fa-check"></i>{" "}
+                    <Link to="#">Graphic Design</Link>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="col-lg-4 col-sm-6 newsletter">
+                <h4>Our Newsletter</h4>
+                <p>
+                  Tamen quem nulla quae legam multos aute sint culpa legam
+                  noster magna
+                </p>
+                <form action="" method="post">
+                  <input type="email" name="email" />
+                  <input type="submit" value="Subscribe" />
+                </form>
+
+                <div className="social-links mt-3">
+                  <Link to="#">
+                    <i className="fab fa-facebook-f"></i>
+                  </Link>
+                  <Link to="#">
+                    <i className="fab fa-twitter"></i>
+                  </Link>
+                  <Link to="#">
+                    <i className="fab fa-linkedin-in"></i>
+                  </Link>
+                  <Link to="#">
+                    <i className="fab fa-instagram"></i>
+                  </Link>
+                  <Link to="#">
+                    <i className="fab fa-pinterest-p"></i>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="footer-bottom">
+          <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <p className="text-center mb-2">
+                  &copy; Copyright <strong>needwork</strong>. All Rights
+                  Reserved
+                </p>
+                <p className="text-center mb-0">
+                  Created by{" "}
+                  <Link to="https://www.needwork.xyz/">needwork.xyz</Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
